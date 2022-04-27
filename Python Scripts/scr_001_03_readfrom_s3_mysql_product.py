60703651e90c53c054dde5f77f2a0506ea16ea88
ALI21139474 <x21139474@student.ncirl.ie>
2022-04-27 23:08:16 +0200

    Initial version of code
    Initial version of code

---
+++ Python Scripts/scr_001_03_readfrom_s3_mysql_product.py
@@ -0,0 +1,54 @@
+import x21139474_readfroms3
+import mysql.connector
+from mysql.connector import Error
+from datetime import datetime
+
+data=x21139474_readfroms3.readfroms3("AKIAX4XNGG7XBMDFDMRE","SCmBBJiBGOmFX7IRD+CtXJjwNH8soUhOotmESGD7","eu-west-1","s3-creditods-landingzone","landing-app1/seq_product_20220417.csv")
+
+
+
+try:
+   connection = mysql.connector.connect(host='creditods.ccrwfsa7yui7.eu-west-1.rds.amazonaws.com',
+                             database='creditODS',
+                             user='svc28021991',
+                             password='NyH58upm#')
+                             #auth_plugin='caching_sha2_password')
+
+   if connection.is_connected():
+       cursor = connection.cursor()
+
+       print("Connection established with MySQL DB creditODS @",datetime.now())
+       #cursor.execute("select creditODS();")
+       #record=cursor.fetchone()
+       #print("You're connected to the database : ",database)
+   
+       cursor.execute("TRUNCATE TABLE creditODS.STG_PRODUCT") 
+       for i,row in data.iterrows():
+    
+            sql_insert_Query = "INSERT INTO creditODS.STG_PRODUCT VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            cursor.execute(sql_insert_Query,tuple(row))
+            connection.commit()
+    
+       print("Total Records Inserted:",i+1)
+
+       sql_select_Query = "select * from creditODS.STG_PRODUCT;"
+       cursor.execute(sql_select_Query)
+       records = cursor.fetchall()
+
+       print("Total record count in creditODS.STG_PRODUCT:", cursor.rowcount)
+       #print ("Printing each developer record")
+   
+       #for row in records:
+       # print (row)
+
+       cursor.close()
+       print("Connection closed with MySQL DB CreditODS @ ",datetime.now())
+
+except Error as e :
+    print ("Error while connecting to MySQL", e)
+finally:
+    #closing database connection.
+    if(connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("connection is closed")
